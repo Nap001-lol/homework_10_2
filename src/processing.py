9dc3714b4a5385ba94dcf11ec02853376265ce91
Samurai <samurai@example.com>
2026-07-26 17:55:48 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,4 @@
-"""Принимает список словарей и опционально значение для ключа state
- (по умолчанию 'EXECUTED') и возвращает новый список словарей, содержащий только те словари, у которых ключ state
- соответствует указанному значению"""
+from datetime import datetime
 
 def filter_by_state(date_list, state='EXECUTED'):
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
@@ -12,8 +10,19 @@ def filter_by_state(date_list, state='EXECUTED'):
     return new_list
 
 
+def sort_by_date(date_list, date_reverse=True):
+    """Функция возвращает новый список, отсортированный по дате (date)"""
+    # for i in date_list:
+    #     return sorted(date_list, key=i['date'], reverse=date)
+    return sorted(date_list, key=lambda i: datetime.fromisoformat(i['date']), reverse=date_reverse)
+
+
 if __name__ == "__main__":
     print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], 'CANCELED'))
+    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
