da2e1e9d4f1a18bbf7a75ef73e8c20f7a058f83f
Samurai <samurai@example.com>
2026-07-30 22:00:25 +0300

    fix variables

--- src/widget.py
+++ src/widget.py
@@ -15,27 +15,27 @@ from masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_data: str) -> str:
     """Функция, котрая обрабатывает информацию о картах(счетах)"""
 
-    list_card_data = card_data.split()
-    mask_card_data = []
-    for i in list_card_data:
+    card_data_parts = card_data.split()
+    masked_data = []
+    for i in card_data_parts:
         if i.isalpha():
-            mask_card_data.append(i)
+            masked_data.append(i)
         elif i.isdigit():
             if len(i) == 16:
                 list_digit_mask_card = get_mask_card_number(i)
-                mask_card_data.append(list_digit_mask_card)
+                masked_data.append(list_digit_mask_card)
             elif len(i) == 20:
-                mask_card_data.append(get_mask_account(i))
+                masked_data.append(get_mask_account(i))
             else:
                 return "Введены некоректные данные"
         else:
             return "Введены некоректные данные"
-    return " ".join(mask_card_data)
+    return " ".join(masked_data)
 
 
 def get_data(iso_string: str) -> str:
-    str_string_data = datetime.fromisoformat(iso_string)
-    return str(str_string_data)[8:10] + "." + str(str_string_data)[5:7] + "." + str(str_string_data)[:4]
+    str_data = datetime.fromisoformat(iso_string)
+    return str(str_data)[8:10] + "." + str(str_data)[5:7] + "." + str(str_data)[:4]
 
 
 if __name__ == "__main__":
