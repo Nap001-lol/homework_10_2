9382d846861790bfed76e715f3d51cd4ea5da003
Samurai <samurai@example.com>
2026-08-26 18:49:51 +0300

    add conftest.py and work in test_generators.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,7 +46,7 @@ def transactions():
             "description": "Перевод организации",
             "from": "Счет 75106830613657916938",
             "to": "Счет 11776614605963066798"
-        }
+        },
         {
             "id": 142264288,
             "state": "FAMOUS",
@@ -63,3 +63,36 @@ def transactions():
             "to": "Счет 75651667383060284189"
         }
     ]
+
+
+
+@pytest.fixture
+def result_next_1():
+    return {
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      }
+
+@pytest.fixture
+def result_next_2():
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
