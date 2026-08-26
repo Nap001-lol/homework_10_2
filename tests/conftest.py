4a8dc85da8efa80974f65f5358131010604a274c
Samurai <samurai@example.com>
2026-08-26 18:32:24 +0300

    add conftest.py and work in test_generators.py

---
+++ tests/conftest.py
@@ -0,0 +1,65 @@
+import pytest
+
+@pytest.fixture
+def transactions():
+    return [{
+          "id": 939719570,
+          "state": "EXECUTED",
+          "date": "2018-06-30T02:08:58.425572",
+          "operationAmount": {
+              "amount": "9824.07",
+              "currency": {
+                  "name": "USD",
+                  "code": "USD"
+              }
+          },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+      },
+      {
+              "id": 142264268,
+              "state": "EXECUTED",
+              "date": "2019-04-04T23:20:05.206878",
+              "operationAmount": {
+                  "amount": "79114.93",
+                  "currency": {
+                      "name": "USD",
+                      "code": "USD"
+                  }
+              },
+              "description": "Перевод со счета на счет",
+              "from": "Счет 19708645243227258542",
+              "to": "Счет 75651667383060284188"
+       },
+        {
+            "id": 789789545,
+            "state": "FAMOUS",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "SSD",
+                    "code": "SSD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916938",
+            "to": "Счет 11776614605963066798"
+        }
+        {
+            "id": 142264288,
+            "state": "FAMOUS",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "SSD",
+                    "code": "SSD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258543",
+            "to": "Счет 75651667383060284189"
+        }
+    ]
